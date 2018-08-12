20d1033aea73033705de94b2ea47d0581f49f090
Ido Zilberberg <idoz@orbs.com>
2018-08-12 11:40:15 +0300

    WIP first test lean helix

---
+++ services/consensusalgo/leanhelix/test/pbft_test.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestShouldSendPreprepareOnlyIfLeader(t *testing.T) {
+
+	/*
+	testNetwork, blockUtils := aSimpleTestNetwork()
+
+	mockGossips := make([]Gossip, len(testNetwork.nodes))
+
+	sync := new(chan bool)
+
+
+	//const node0 = testNetwork.nodes[0]
+	//const node1 = testNetwork.nodes[1]
+	//const node2 = testNetwork.nodes[2]
+	//const node3 = testNetwork.nodes[3]
+	//const gossip0 = testNetwork.getNodeGossip(node0.pk);
+	//const gossip1 = testNetwork.getNodeGossip(node1.pk);
+	//const gossip2 = testNetwork.getNodeGossip(node2.pk);
+	//const gossip3 = testNetwork.getNodeGossip(node3.pk);
+	//const spy0 = sinon.spy(gossip0, "multicast");
+	//const spy1 = sinon.spy(gossip1, "multicast");
+	//const spy2 = sinon.spy(gossip2, "multicast");
+	//const spy3 = sinon.spy(gossip3, "multicast");
+
+	testNetwork.StartConsensusOnAllNodes(&sync)
+	//<- sync
+
+	blockUtils.provideNextBlock(&sync)
+	blockUtils.ResolveAllValidations(true)
+
+	// <- sync
+	//await nextTick(); // await for notifyCommitted
+	const preprepareCounter = (spy: sinon.SinonSpy) => spy.getCalls().filter(c => c.args[1] === "preprepare").length;
+
+	expect(preprepareCounter(spy0)).to.equal(1);
+	expect(preprepareCounter(spy1)).to.equal(0);
+	expect(preprepareCounter(spy2)).to.equal(0);
+	expect(preprepareCounter(spy3)).to.equal(0);
+
+	testNetwork.shutDown()
+*/
+
+}
